be8ca7aa50fd192db6ac9b0bb0a7e79f4e36a715
iitjee <iit.jee.help.me@gmail.com>
2017-02-04 00:20:07 +0530

    Create 00 Intro.r

---
+++ LogisticRegression/00 Intro.r
@@ -0,0 +1,25 @@
+/*
+
+
+ here's what you can expect to learn. The characteristics of binomial outcome variables, such as buys versus doesn't buy, 
+ and recovers or get worse, any kind of situation which you might have to make a decision between two or more alternatives
+ is an appropriate kind of context for this course. You'll also learn about the reasons that binomial outcome variables create
+ problems for standard analysis procedures such as simple and multiple regression.
+
+You'll see how simple transformations that involve odds ratios and logarithms can solve the problems that binomial outcomes
+bring to regression analysis. Now I use Excel in this course mainly to show you what goes on inside logistic regression's 
+black box. You'll be able to see the intermediate calculations involving those odds and those logarithms, and I use R to show 
+you how to reach the end point of the analysis quickly so that you can skip over those intermediate kinds of calculations.
+
+
+
+*/
+
+
+
+
+
+
+
+
+
