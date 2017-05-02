e3c0440191343893072433ca1b6471d475b86bf4
iitjee <iit.jee.help.me@gmail.com>
2017-05-02 01:33:35 -0700

    Create 00 Basics.r

---
+++ R LANGUAGE/00 Basics.r
@@ -0,0 +1,28 @@
+A data frame is a list with class "data.frame"
+
+#making dataframe
+accountants <- data.frame(home=statef, loot=incomes, shot=incomef)
+
+The simplest way to construct a data frame from scratch is to use the read.table() function
+to read an entire data frame from an external file.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
